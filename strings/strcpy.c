aa8155c0b71ed3e727db5599713b97636739bdc1
deeku124 <94981044+deeku124@users.noreply.github.com>
2024-02-05 17:07:10 +0530

    Update strcpy.c

--- strings/strcpy.c
+++ strings/strcpy.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<string.h>
-char *str_cpy(char str1[],char str2[])
+char *str_cpy(char *str1,char *str2)
 { 
 int i=0;
-while((str1[i]=str2[i])!='\0')
-i++;
+while((*str1=*str2)!='\0')
+{
+str1++;
+str2++;
+}
 return str1;
 }
 
@@ -13,13 +16,10 @@ int main()
 {
 char str1[10],str2[10];
 printf("enter first string\n");
-//scanf("%s",&str1);
-gets(str1);
+scanf("%s",str1);
 printf("enter second string\n");
-//scanf("%s",&str2);
-gets(str2);
+scanf("%s",str2);
 char *str3=str_cpy(str1,str2);
-//printf("%s\n",str3);
-puts(str3);
+printf("%s\n",str3);
 return 0;
 }
