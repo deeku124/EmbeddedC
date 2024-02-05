c8e456a548b39bfaab0f88dbb73ae255f483b81f
deeku124 <94981044+deeku124@users.noreply.github.com>
2024-02-05 16:41:15 +0530

    Update strcat.c

--- strings/strcat.c
+++ strings/strcat.c
@@ -14,9 +14,9 @@ int main()
 {
 char str1[10],str2[10];
 printf("enter first string\n");
-scanf("%s",&str1);
+scanf("%s",str1);
 printf("enter second string\n");
-scanf("%s",&str2);
+scanf("%s",str2);
 char *str3=str_cat(str1,str2);
 printf("%s\n",str3);
 return 0;
