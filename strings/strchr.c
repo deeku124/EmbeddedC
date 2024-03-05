f0ab2c853f00a6fbf2cea27ed17fc3512c5f9656
deeku124 <94981044+deeku124@users.noreply.github.com>
2024-03-05 15:15:01 +0530

    Create strchr.c

---
+++ strings/strchr.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+char *str_chr(char *str,char ch)
+{
+int i=0;
+char *ptr;
+while(i!=strlen(str))
+{
+if(*(str+i)==ch)
+{
+ptr=(str+i);
+return ptr;
+}
+i++;
+}
+
+int main()
+{
+char str[15];
+printf("enter string\n");
+scanf("%s",str);
+printf("enter character\n");
+scanf(" %c",&ch);
+char *str1=str_chr(str,ch);
+printf("%s",str1);
+return 0;
+}
